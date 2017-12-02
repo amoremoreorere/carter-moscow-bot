f6dcb602cc76324ac249d4958da52c69a05df442
amoremoreorere <33686731+amoremoreorere@users.noreply.github.com>
2017-12-02 20:19:02 +0300

    Update carter-moscow-bot.py

--- carter-moscow-bot.py
+++ carter-moscow-bot.py
@@ -1,27 +1,18 @@
-from telegram.ext import Updater
-from telegram.ext import CommandHandler
-import logging
-import os
+# -*- coding: utf-8 -*-
 import telebot
 import requests
 from telebot import types
 
-TOKEN = os.environ['TOKEN']
+TOKEN = '443038853:AAGkWWhT0toBHvyniNc0VAShL1ZNBhbSRYU'
+bot = telebot.TeleBot(TOKEN)
 
-def start(bot, update):
-  bot.sendMessage(chat_id=update.message.chat_id, text="I'm a bot, please talk to me!")
+@bot.message_handler(commands=['start'])
+def start(message):
+    sent = bot.send_message(message.chat.id, message.from_user.first_name + ', Добро пожаловать! Здесь ты можешь подобрать одежду своему малышу.\nКому ищешь? \nЕсли мальчику, то жми /boy. \nЕсли девочке, то жми /girl. \n Хочешь узнать историю бренда Carters, жми /history \nЧтобы узнать о доставке, жми /dostavka \nУзнать о размерах Картер, жми /size_chart \nКоманда /help ознакомит тебя со всеми моими возможностями и командами \nЕсли живешь в Строгино, жми /strogino')
 
-def echo(bot, update):
-  bot.sendMessage(chat_id=update.message.chat_id, text=update.message.text)
+@bot.message_handler(commands=['help'])
+def start(message):
+    sent = bot.send_message(message.chat.id, message.from_user.first_name + ', я помогу тебе здесь разобраться. Вот команды, которые я знаю: \n \start - чтобы начать выбор одежды  \n/zakaz - чтобы сделать заказ \n/dostavka - узнать о доставке \n/history - узнать историю бренда Картерс \n/size_chart - определить размер одежды малыша')
 
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    level=logging.INFO)
-
-updater = Updater(TOKEN)
-dispatcher = updater.dispatcher
-
-start_handler = CommandHandler('start', start)
-
-dispatcher.add_handler(start_handler)
-
-updater.start_polling()
+if __name__ == '__main__':
+    bot.polling(none_stop=True)
