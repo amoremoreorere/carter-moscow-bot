e576d824754a2f080c392d81f3a92ff74c67b4e2
amoremoreorere <33686731+amoremoreorere@users.noreply.github.com>
2017-12-02 16:47:57 +0300

    Update carter-moscow-bot.py

--- carter-moscow-bot.py
+++ carter-moscow-bot.py
@@ -1,21 +1,77 @@
 # -*- coding: utf-8 -*-
-import telebot
-import requests
-from telebot import types
-import os
-import sys
 
-
+import requests  
+import datetime
 
 TOKEN = '443038853:AAGkWWhT0toBHvyniNc0VAShL1ZNBhbSRYU'
-bot = telebot.TeleBot(TOKEN)
+url = "https://api.telegram.org/bot443038853:AAGkWWhT0toBHvyniNc0VAShL1ZNBhbSRYU/"
+ 
+class BotHandler:
+ 
+    def __init__(self, token):
+        self.token = token
+        self.api_url = "https://api.telegram.org/bot{}/".format(token)
+ 
+    def get_updates(self, offset=None, timeout=30):
+        method = 'getUpdates'
+        params = {'timeout': timeout, 'offset': offset}
+        resp = requests.get(self.api_url + method, params)
+        result_json = resp.json()['result']
+        return result_json
+ 
+    def send_message(self, chat_id, text):
+        params = {'chat_id': chat_id, 'text': text}
+        method = 'sendMessage'
+        resp = requests.post(self.api_url + method, params)
+        return resp
+ 
+    def get_last_update(self):
+        get_result = self.get_updates()
+ 
+        if len(get_result) > 0:
+            last_update = get_result[-1]
+        else:
+            last_update = get_result[len(get_result)]
+ 
+        return last_update
 
-@bot.message_handler(commands=['start'])
-def start(message):
-    sent = bot.send_message(message.chat.id, message.from_user.first_name + ', Добро пожаловать! Здесь ты можешь подобрать одежду своему малышу.\nКому ищешь? \nЕсли мальчику, то жми /boy. \nЕсли девочке, то жми /girl. \n Хочешь узнать историю бренда Carters, жми /history \nЧтобы узнать о доставке, жми /dostavka \nУзнать о размерах Картер, жми /size_chart \nКоманда /help ознакомит тебя со всеми моими возможностями и командами \nЕсли живешь в Строгино, жми /strogino')
-
-@bot.message_handler(commands=['help'])
-def start(message):
-    sent = bot.send_message(message.chat.id, message.from_user.first_name + ', я помогу тебе здесь разобраться. Вот команды, которые я знаю: \n \start - чтобы начать выбор одежды  \n/zakaz - чтобы сделать заказ \n/dostavka - узнать о доставке \n/history - узнать историю бренда Картерс \n/size_chart - определить размер одежды малыша')
-
-if __name__ == "__main__":
+greet_bot = BotHandler(token)  
+greetings = ('hello', 'hi', 'greetings', 'sup')  
+now = datetime.datetime.now()
+ 
+ 
+def main():  
+    new_offset = None
+    today = now.day
+    hour = now.hour
+ 
+    while True:
+        greet_bot.get_updates(new_offset)
+ 
+        last_update = greet_bot.get_last_update()
+ 
+        last_update_id = last_update['update_id']
+        last_chat_text = last_update['message']['text']
+        last_chat_id = last_update['message']['chat']['id']
+        last_chat_name = last_update['message']['chat']['first_name']
+ 
+        if last_chat_text.lower() in greetings and today == now.day and 6 <= hour < 12:
+            greet_bot.send_message(last_chat_id, 'Good Morning  {}'.format(last_chat_name))
+            today += 1
+ 
+        elif last_chat_text.lower() in greetings and today == now.day and 12 <= hour < 17:
+            greet_bot.send_message(last_chat_id, 'Good Afternoon {}'.format(last_chat_name))
+            today += 1
+ 
+        elif last_chat_text.lower() in greetings and today == now.day and 17 <= hour < 23:
+            greet_bot.send_message(last_chat_id, 'Good Evening  {}'.format(last_chat_name))
+            today += 1
+ 
+        new_offset = last_update_id + 1
+ 
+if __name__ == '__main__':  
+    try:
+        main()
+    except KeyboardInterrupt:
+        exit()
+    
